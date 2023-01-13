cdd23bf5f21d43e6b726df7a64afccfe26012bd7
Andrew Cornejo <atcornejo3@gmail.com>
2023-01-12 17:31:57 -0800

    fleshed out home footer and header components

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import Preview from '../Preview/Preview';
+import Editor from '../Editor/Editor';
+// import { useState } from 'react';
+
+import './Home.css';
+
+export default function Home() {
+  return (
+    <main>
+      <Preview />
+      <Editor />
+    </main>
+  );
+}
